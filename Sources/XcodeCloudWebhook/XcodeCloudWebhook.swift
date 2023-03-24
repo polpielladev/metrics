ec94acbd7915add506663d2935a1d8f9a44e83e5
Pol Piella Abadia <hi@polpiella.dev>
2023-03-24 17:00:01 +0000

    Updates struct to fetch id too

--- Sources/XcodeCloudWebhook/XcodeCloudWebhook.swift
+++ Sources/XcodeCloudWebhook/XcodeCloudWebhook.swift
@@ -1,6 +1,41 @@
 import AWSLambdaRuntime
 import AWSLambdaEvents
 
+struct XcodeWebhookBody: Decodable {
+    let ciWorkflow: CIWorkflow
+    let ciBuildRun: CIBuildRun
+    let scmRepository: SCMRepository
+    let scmGitReference: SCMGitReference
+    
+    struct CIWorkflow: Decodable {
+        let attributes: Attributes
+        
+        struct Attributes: Decodable {
+            let name: String
+        }
+    }
+    
+    struct CIBuildRun: Decodable {
+        let id: String
+    }
+    
+    struct SCMRepository: Decodable {
+        let attributes: Attributes
+        
+        struct Attributes: Decodable {
+            let repositoryName: String
+        }
+    }
+    
+    struct SCMGitReference: Decodable {
+        let attributes: Attributes
+        
+        struct Attributes: Decodable {
+            let name: String
+        }
+    }
+}
+
 @main
 struct XcodeCloudWebhook: SimpleLambdaHandler {
     func handle(_ request: APIGatewayV2Request, context: LambdaContext) async throws -> APIGatewayV2Response {
